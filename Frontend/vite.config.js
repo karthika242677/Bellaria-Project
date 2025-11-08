baf45c47ebd48bf25b8e37f28b7e68937e46ecbe
karthika242677 <karthikacs810@gmail.com>
2025-11-08 16:05:19 +0530

    commit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   server : {
+    port : 5000 , 
+    host : '0.0.0.0' , 
+    allowedHosts : ['bellaria-project-3.onrender.com']
+  }
   
 })
